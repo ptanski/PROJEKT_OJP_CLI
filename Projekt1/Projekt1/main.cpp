d60f74e430821ce14c5ac1ff159d7c1bf5a7110a
Przemek Tański <przemek.tanski@gmail.com>
2015-05-17 22:58:17 +0200

    Added first window (empty for now)

--- Projekt1/Projekt1/main.cpp
+++ Projekt1/Projekt1/main.cpp
@@ -1,7 +1,11 @@
+#include <Windows.h>
 #include <iostream>
+#include "MyForm.h"
 
 int main()
 {
-	std::cerr << "something went bad" << std::endl;
+	std::cout << "wyjscie do konsoli" << std::endl;
+	Projekt1::MyForm form1;
+	System::Windows::Forms::Application::Run(%form1);
 	return -1;
 }
